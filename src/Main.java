dbd48aa99631f35475d8a9a55bd2666c07f53428
ruslanzverkov <createmed@mail.ru>
2022-09-24 19:04:56 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -3,54 +3,45 @@ import java.util.*;
 import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.iterator;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 6, 5, 4,12, 6, 8));
-//задание 1
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("Машина", "дорога", "Машина", "корова", "дорога", "Машина"));
 
+    public static void main(String[] args) {
+        //sort1();
+        sort2();
+        sort3();
+        sort4();
+
+    }
+
+
+    public static void sort1() {
         for (Integer num : nums) {
             if (num % 2 != 0) {
                 System.out.print(num + " ");
             }
         }
-        System.out.println();
+    }
 
-        //задание 2
-
-        for (int i = 0; i < nums.size()-1; i++) {
-            Collections.sort(nums);
-            if (nums.get(i) % 2 == 0& !Objects.equals(nums.get(i), nums.get(i + 1))) {
-                System.out.print(nums.get(i)+" ");
+    // не знаю почему данный способ у меня не работает sort2
+    public static void sort2() {
+        Collections.sort(nums);
+        int prevnum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevnum) {
+                System.out.println(num);
+                num = prevnum;
             }
         }
-        if (nums.get(nums.size()-1)%2==0) {
-            System.out.println(nums.get(nums.size()-1));
-        }
-        //Задание 3
-        List<String> strings=new ArrayList<>(List.of("Машина","дорога","Машина","корова","дорога","Машина"));
+    }
 
-        for (int i = 0; i < strings.size()-1; i++) {
-            Collections.sort(strings);
-            if ( !Objects.equals(strings.get(i), strings.get(i + 1))) {
-                System.out.print(strings.get(i)+" ");
-            }
-        }
-        if ( !Objects.equals(strings.get(strings.size()-1), strings.get(strings.size()-2))) {
-            System.out.println(strings.get(strings.size()-1)+" ");
-        }
+    public static void sort3() {
+        Set<String> unique=new HashSet<>(strings);
+        System.out.println(unique);
+    }
 
-
-        //Задание 4
-
-        for (int i = 0; i < strings.size()-1; i++) {
-            Collections.sort(strings);
-            int b=1;
-            for (int a = i; a < strings.size()-1; a++) {
-                if ( Objects.equals(strings.get(i), strings.get(a + 1))) {
-                     b=b+1;
-                }
-            }
-            System.out.println(b);
-        }
-
+    public static void sort4() {
+        Set<String> unique=new HashSet<>(strings);
+        System.out.println(strings.size()-unique.size());
     }
 }
